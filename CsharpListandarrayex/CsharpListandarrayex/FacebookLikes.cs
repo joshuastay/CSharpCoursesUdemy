ee3e4a2c36b04a60537ed6787b07c694c78e6288
Joshua Stay <57918075+joshuastay@users.noreply.github.com>
2020-10-16 19:10:46 -0700

    Add files via upload

---
+++ CsharpListandarrayex/CsharpListandarrayex/FacebookLikes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace CsharpListandarrayex
+{
+    public class FacebookLikes
+    {
+        public List<string> likes = new List<string>();
+
+        public void UserLikes(string name)
+        {
+            likes.Add(name);
+        }
+        public int GetLikes()
+        {
+            return likes.Count;
+        }
+    }
+}
---
+++ CsharpListandarrayex/CsharpListandarrayex/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace CsharpListandarrayex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var facebook = new FacebookLikes();
+            string newLike;
+            while (true)
+            {
+                Console.WriteLine("Enter a name: ");
+                newLike = Console.ReadLine();
+                if (String.IsNullOrEmpty(newLike))
+                {
+                    break;
+                }
+                facebook.UserLikes(newLike);
+            }
+            int numberOfLikes = facebook.GetLikes();
+
+            if (numberOfLikes > 2)
+            {
+                Console.WriteLine(String.Format("{0}, {1} and {2} others like your post!",
+                    facebook.likes[0], facebook.likes[1], facebook.GetLikes() - 2));
+            }
+            else if (numberOfLikes == 2)
+            {
+                Console.WriteLine(String.Format("{0} and {1} like your post!",
+                    facebook.likes[0], facebook.likes[1]));
+            }
+            else if (numberOfLikes == 1)
+            {
+                Console.WriteLine(String.Format("{0} likes your post!",
+                    facebook.likes[0]));
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Type Your Name: ");
+            string username = Console.ReadLine();
+            int lengthUser = username.Length;
+            char[] nameArray = new char[lengthUser];
+            foreach (char letter in username)
+            {
+                nameArray[username.IndexOf(letter)] = letter;
+            }
+            Array.Reverse(nameArray);
+            Console.WriteLine(nameArray);
+
+            Console.WriteLine();
+            int[] fiveNums = new int[5];
+            while (true)
+            {
+                Console.WriteLine("Enter 5 numbers: ");
+                var newnums = Console.ReadLine();
+                foreach (char number in newnums)
+                {
+
+                }
+            }
+        }
+    }
+}
